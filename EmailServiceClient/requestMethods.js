0f0a4d80a5c33186aac2ba19ad2dd37e07bc8d31
Ankush Gupta <ankushgupta365@gmail.com>
2023-07-19 12:07:33 +0530

    production environment set

--- EmailServiceClient/requestMethods.js
+++ EmailServiceClient/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const BASE_URL = "http://localhost:8800/api"
-export const PRODUCTION_URL= "http://43.204.119.135:8800/api"
+export const PRODUCTION_URL= "http://13.233.59.136/api"
 
 
 let TOKEN;
@@ -13,9 +13,9 @@ const getToken = ()=>{
 getToken()
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL
+    baseURL: PRODUCTION_URL
 })
 export const userRequest = axios.create({
-    baseURL: BASE_URL,
+    baseURL: PRODUCTION_URL,
     headers: {token: `Bearer ${TOKEN}`}
 })
